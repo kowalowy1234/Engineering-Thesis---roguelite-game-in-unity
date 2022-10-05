1dc3c7d2429b61170cef36dd17b146b0201dfc9c
Piotr <kowalowy1234@gmail.com>
2022-10-05 16:41:50 +0200

    Format and clean up

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -8,27 +8,29 @@ public class PlayerMovement : MonoBehaviour
   LayerMask blinkLayerMask;
 
   public float moveSpeed = 10f;
+
+
   public int energy = 10;
   public float blinkRange = 3f;
   float horizontalMovement;
   float verticalMovement;
+
   bool isBlinking = false;
 
   public Animator animator;
   private SpriteRenderer playerSprite;
+
+
   private Rigidbody2D rigidBody2D;
+
+
   private Vector3 moveDirection;
 
   private void Awake()
   {
     rigidBody2D = GetComponent<Rigidbody2D>();
-  }
-
-  private void Start()
-  {
     playerSprite = gameObject.GetComponent<SpriteRenderer>();
   }
-
 
   private void Update()
   {
@@ -59,38 +61,42 @@ public class PlayerMovement : MonoBehaviour
 
     if (isBlinking)
     {
+      gameObject.layer = LayerMask.NameToLayer("PlayerDodge");
       rigidBody2D.MovePosition(blinkTarget);
       isBlinking = false;
+      gameObject.layer = LayerMask.NameToLayer("Player");
     }
   }
 
   private void GetMoveDir()
   {
+
     horizontalMovement = Input.GetAxisRaw("Horizontal");
     verticalMovement = Input.GetAxisRaw("Vertical");
 
-    float moveX = 0f;
-    float moveY = 0f;
+
+    int moveX = 0;
+    int moveY = 0;
 
     if (horizontalMovement > 0)
     {
-      moveX += 1f;
+      moveX += 1;
     }
     else if (horizontalMovement < 0)
     {
-      moveX -= 1f;
+      moveX -= 1;
     }
 
     if (verticalMovement > 0)
     {
-      moveY += 1f;
+      moveY += 1;
     }
     else if (verticalMovement < 0)
     {
-      moveY -= 1f;
+      moveY -= 1;
     }
 
-    // vector needs to be normalized so the player's movement speed is alwaus the same (length of vector is always 1)
+    // vector needs to be normalized so the player's movement speed is always the same on main axis and diagonally (length of vector is always 1)
     moveDirection = new Vector3(moveX, moveY).normalized;
   }
 }
