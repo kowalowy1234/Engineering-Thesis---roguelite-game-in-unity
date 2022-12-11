03ae8334718bcba5b12622c9e7a5d1bf3a56de7f
Piotr <kowalowy1234@gmail.com>
2022-12-11 17:40:31 +0100

    Victory screen animations

---
+++ Assets/Scripts/UIScripts/VictoryScreen/MoveAndFinish.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveAndFinish : MonoBehaviour
+{
+  public float y = 1350f;
+  public float moveSpeed = 1f;
+  public RectTransform rectTransform;
+
+  // Update is called once per frame
+  void Update()
+  {
+    Debug.Log(rectTransform.position.y);
+    if (rectTransform.position.y >= y)
+    {
+      SceneManager.LoadScene("Main menu");
+    }
+
+    if (Input.GetKeyUp(KeyCode.Escape))
+    {
+      SceneManager.LoadScene("Main menu");
+    }
+
+    rectTransform.position = new Vector3(rectTransform.position.x, rectTransform.position.y + moveSpeed * Time.deltaTime, 0f);
+  }
+}
